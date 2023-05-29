7f6f4c13a829af6119d482839d427f0c1a741213
David Lonyai <david.lonyai@gmail.com>
2023-05-29 16:09:12 +0200

    feature: build & release npm (#2)

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,11 +1,46 @@
 import * as DNT from "https://deno.land/x/dnt@0.21.2/mod.ts";
 import { parse } from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import { join } from "https://deno.land/std@0.146.0/path/mod.ts";
-import * as T from "../task_either.ts";
+import * as T from "../async_either.ts";
 import * as D from "../decoder.ts";
 import * as A from "../array.ts";
 import * as E from "../either.ts";
-import { flow, pipe } from "../fns.ts";
+import { flow, pipe } from "../fn.ts";
+
+// Defaults
+const gitVersion = await new Deno.Command("git", {
+  args: ["describe", "--abbrev=0", "--tags"],
+})
+  .output()
+  .then((out) => new TextDecoder().decode(out.stdout).trim());
+
+const version = parse(gitVersion) ?? parse("0.0.0")!;
+
+const files = Array.from(Deno.readDirSync(Deno.cwd()))
+  .filter((file) => file.name.endsWith(".ts"))
+  .map((file) => file.name)
+  .sort()
+  .map((path) => ({
+    name: path.replace(".ts", ""),
+    path,
+  }));
+
+// console.log(files);
+
+const defualts: [string, string][] = [
+  ["NAME", "fun"],
+  ["DESCRIPTION", "fun"],
+  ["VERSION", version.toString()],
+  ["BUILD_DIR", "npm"],
+  ["ENTRYPOINTS", JSON.stringify(["mod.ts"])],
+  ["ADDITIONAL_FILES", JSON.stringify(["README.md", "LICENSE"])],
+];
+
+defualts.forEach(([key, value]) => {
+  if (!Deno.env.has(key)) {
+    Deno.env.set(key, value);
+  }
+});
 
 // Environment
 const semver = pipe(
@@ -15,7 +50,7 @@ const semver = pipe(
     return semVer === null
       ? D.failure(i, "Semantic Version")
       : D.success(semVer);
-  }),
+  })
 );
 
 const Env = D.struct({
@@ -23,34 +58,37 @@ const Env = D.struct({
   DESCRIPTION: D.string,
   VERSION: semver,
   BUILD_DIR: D.string,
-  ENTRYPOINTS: D.json(D.array(D.string)),
+  ENTRYPOINTS: D.alt(D.json(D.array(D.string)))(
+    D.json(D.array(D.struct({ name: D.string, path: D.string })))
+  ),
   ADDITIONAL_FILES: D.json(D.array(D.string)),
 });
 
-type Env = D.TypeOf<typeof Env>;
+type Env = typeof Env extends D.Decoder<infer I, infer O> ? O : never;
 
 // Errors
 type BuildError = { message: string; context: Record<string, unknown> };
 
 const buildError = (
   message: string,
-  context: Record<string, unknown> = {},
+  context: Record<string, unknown> = {}
 ): BuildError => ({ message, context });
 
 const printBuildError = ({ message, context }: BuildError) => {
   let msg = `BUILD ERROR: ${message}\n`;
-  msg += Object
-    .entries(context)
+  msg += Object.entries(context)
     .map(([key, value]) => {
-      const val = typeof value === "string"
-        ? value
-        : value instanceof Error
-        ? value
-        : typeof value === "object" && value !== null &&
+      const val =
+        typeof value === "string"
+          ? value
+          : value instanceof Error
+          ? value
+          : typeof value === "object" &&
+            value !== null &&
             Object.hasOwn(value, "toString") &&
             typeof value.toString === "function"
-        ? value.toString()
-        : JSON.stringify(value, null, 2);
+          ? value.toString()
+          : JSON.stringify(value, null, 2);
       return `Context - ${key}\n${val}`;
     })
     .join("\n");
@@ -58,9 +96,13 @@ const printBuildError = ({ message, context }: BuildError) => {
 };
 
 // Functions
-const createBuildOptions = (
-  { NAME, DESCRIPTION, BUILD_DIR, VERSION, ENTRYPOINTS }: Env,
-): DNT.BuildOptions => ({
+const createBuildOptions = ({
+  NAME,
+  DESCRIPTION,
+  BUILD_DIR,
+  VERSION,
+  ENTRYPOINTS,
+}: Env): DNT.BuildOptions => ({
   entryPoints: ENTRYPOINTS.slice(),
   outDir: BUILD_DIR,
   typeCheck: false,
@@ -73,42 +115,63 @@ const createBuildOptions = (
     version: VERSION.toString(),
     description: DESCRIPTION,
     license: "MIT",
+    publishConfig: {
+      "@type-driven:registry": "https://npm.pkg.github.com",
+    },
+    exports: {
+      ".": {
+        import: "./esm/mod.js",
+        require: "./script/mod.js",
+        types: "./types/mod.d.ts",
+      },
+      "./*": {
+        import: "./esm/*.js",
+        require: "./script/*.js",
+        types: "./types/*.d.ts",
+      },
+    },
+    typesVersions: {
+      "<4.7": {
+        // Maybe this won't be correct in the future (use "*" to apply for all TS versions)
+        "index.d.ts": ["types/mod.d.ts"],
+        ...files.reduce((acc, { name }) => {
+          acc[`${name}.d.ts`] = [`types/${name}.d.ts`];
+          return acc;
+        }, <Record<string, string[]>>{}),
+      },
+    },
   },
 });
 
-const getEnv = T.tryCatch(
-  Deno.env.toObject,
-  (err, args) => buildError("Unable to get environment.", { err, args }),
+const getEnv = T.tryCatch(Deno.env.toObject, (err, args) =>
+  buildError("Unable to get environment.", { err, args })
 )();
 
 const parseEnv = flow(
   Env,
   D.extract,
   E.mapLeft((err) => buildError("Unable to parse environment.", { err })),
-  T.fromEither,
+  T.fromEither
 );
 
-const emptyDir = T.tryCatch(
-  DNT.emptyDir,
-  (err, args) => buildError("Unable to empty build directory.", { err, args }),
+const emptyDir = T.tryCatch(DNT.emptyDir, (err, args) =>
+  buildError("Unable to empty build directory.", { err, args })
 );
 
-const build = T.tryCatch(
-  DNT.build,
-  (err, args) => buildError("Unable to build node package.", { err, args }),
+const build = T.tryCatch(DNT.build, (err, args) =>
+  buildError("Unable to build node package.", { err, args })
 );
 
-const copyFile = T.tryCatch(
-  Deno.copyFile,
-  (err, args) => buildError("Unable to copy file.", { err, args }),
+const copyFile = T.tryCatch(Deno.copyFile, (err, args) =>
+  buildError("Unable to copy file.", { err, args })
 );
 
-const traverse = A.traverse(T.Applicative);
+const traverse = A.traverse(T.MonadAsyncEitherParallel);
 
 const copy = ({ BUILD_DIR, ADDITIONAL_FILES }: Env) =>
   pipe(
     ADDITIONAL_FILES,
-    traverse((file) => copyFile(file, join(BUILD_DIR, file))),
+    traverse((file) => copyFile(file, join(BUILD_DIR, file)))
   );
 
 const printComplete = (env: Env) =>
@@ -121,10 +184,10 @@ export const run = pipe(
   T.chainFirst((env) => emptyDir(env.BUILD_DIR)),
   T.chainFirst((env) => build(createBuildOptions(env))),
   T.chainFirst(copy),
-  T.fold(
+  T.match(
     flow(printBuildError, console.error),
-    flow(printComplete, console.log),
-  ),
+    flow(printComplete, console.log)
+  )
 );
 
 await run();
